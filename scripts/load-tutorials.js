1cccee434ee7ce89bdd959ee23fc59027d3cee18
Diana <me@diana.nu>
2024-12-16 16:18:24 -0800

    remove

--- scripts/load-tutorials.js
+++ scripts/load-tutorials.js
@@ -1,10 +1,7 @@
 import loadList from '/scripts/load-list.js';
 
 const tutorials = [
-  // pinned tutorials
   'how-to-join-mysticwarez',
-
-  // all other tutorials
 ];
 document.addEventListener('DOMContentLoaded', () => {
   loadList(tutorials, '/tutorials', '#tutorials');
